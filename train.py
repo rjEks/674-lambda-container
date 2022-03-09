6ad3cd3a43eb485f21edb090d5bf36e25123c5b5
root <root@DESKTOP-TL4JVK4.localdomain>
2022-03-09 20:29:15 -0300

    add files

---
+++ train.py
@@ -0,0 +1,11 @@
+from sklearn import svm, datasets
+import joblib
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+model = svm.SVC(kernel='poly', degree=3, C=1.0).fit(X,y)
+
+joblib.dump(model, "modelo.joblib")
